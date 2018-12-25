67ef80457afac8b7937c7682c7a5c213e2288697
Graham Bevan <graham.bevan@ntlworld.com>
2018-12-25 19:59:46 +0000

    cleanup

--- ui/js/action/index.js
+++ ui/js/action/index.js
@@ -619,39 +619,6 @@ class Action extends Component {
     };
   }
 
-  // vault(path, method, data, headers) {
-  //   headers = headers || {}
-  //   if (!headers['X-Vault-Token']) {
-  //     headers['X-Vault-Token'] = this.props.vaultAuth.token;
-  //   }
-  //
-  //   return fetch(this.props.URLs.vault + '/' + path, {
-  //     headers,
-  //     method: method || 'GET',
-  //     body: data ? JSON.stringify(data) : undefined
-  //   })
-  //   .then((res) => {
-  //     switch(res.status) {
-  //       case 200:
-  //         return res.json();
-  //       case 204:
-  //         return;
-  //       default:
-  //         return Promise.reject(
-  //           new Error(`Request failed with status: ${res.status} ${res.statusText} [${res.url}]`)
-  //         );
-  //     }
-  //   });
-  // }
-
-  // gostint(path, method, data, headers) {
-  //   return fetch(this.props.URLs.gostint + '/' + path, {
-  //     headers,
-  //     method: method || 'GET',
-  //     body: data ? JSON.stringify(data) : undefined
-  //   });
-  // }
-
   advancedSimple() {
     this.setState((state, props) => {
       return {advancedForm: !state.advancedForm};
